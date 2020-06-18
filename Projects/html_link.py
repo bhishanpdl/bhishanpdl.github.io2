3127688dc000c9fb2cf707c5cb46b7f3c28cc726
bpJedisim <bhishantryphysics@gmail.com>
2020-06-18 11:06:54 -0400

    project prudential

--- Projects/html_link.py
+++ Projects/html_link.py
@@ -1,36 +1,33 @@
 import os
 import glob
+import sys
 
-all_projects = []
+folder = sys.argv[1]
+
 
 template = """\
             <h4><a class="mysub" style="text-decoration:none;color:blue" href="{}" target="_blank">
             <strong> {} </strong></a></h4>
 """
 
-folders = []
-for html in glob.glob('*/*.html'):
-    folder = html.split('/')[0]
-    folders.append(folder)
 
-folders = list(set(folders))
-for folder in folders:
-    head = "            Project: {}".format(folder)
-    tail = """\
+one_project = []
+
+
+head = "            Project: {}".format(folder)
+tail = """\
 
 
             <br />
             <br />"""
-    one_project = []
-    for html in sorted(glob.glob(folder + '/*.html')):
-        base = html.split('/')[1].rstrip('.html')
-        out = template.format(html,base)
-        one_project.append(out)
-    one_project = head + '\n' + '\n'.join(one_project) + tail
-    all_projects.append(one_project)
 
-all_projects = '\n'.join(all_projects)
+for html in sorted(glob.glob(folder + '*.html')):
+    base = html.split('/')[1].rstrip('.html')
+    out = template.format(html,base)
+    one_project.append(out)
+one_project = head + '\n' + '\n'.join(one_project) + tail
+
 
 # write output
 with open('output.html','w') as fo:
-    fo.write(all_projects)
+    fo.write(one_project)
